f0be05df0b89c2a725730346359aa176ba6a4b77
Ayushi Shah <as17726@nyu.edu>
2023-09-23 11:01:59 -0400

    Attempt 1

--- sketch.js
+++ sketch.js
@@ -1,6 +1,21 @@
+let maxDiam = 50;
+let minDiam = maxDiam / 10;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(255);
+  strokeWeight(0);
+  fill("red");
+  randomSeed(100);
+
+  for (let xpos = 0; xpos <= windowWidth; xpos += maxDiam) {
+    for (let ypos = 0; ypos <= windowHeight; ypos += maxDiam) {
+      //let x = random(minDiam, maxDiam);
+      //let y = random (minDiam, maxDiam);
+      ellipse(xpos, ypos, 10, 10);
+    }
+  }
 }
